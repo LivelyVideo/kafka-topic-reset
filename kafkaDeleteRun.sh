510a7fbc346a41c00f73af0c83a0faeb7ffa3889
Mike Brennan <mike@livelyvideo.tv>
2021-07-20 15:08:06 -0500

    Adding specifics for topic deletion

--- kafkaDeleteRun.sh
+++ kafkaDeleteRun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Deleting topic ${TOPIC}"
 kafka-topics --bootstrap-server kafka-headless:9092 --topic ${TOPIC} --delete
-sleep 20
-echo "Creating topic ${TOPIC}"
+echo "Waiting for topic to be deleted from cluster..."
+sleep 1000
+echo "Creating topic ${TOPIC} with ${PARTITIONS} partitions and ${REPLICATION} replications"
 kafka-topics --bootstrap-server kafka-headless:9092 --topic ${TOPIC} --partitions ${PARTITIONS} --replication-factor ${REPLICATION} --create
 echo "Finished"
